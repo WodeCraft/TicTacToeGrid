a0a13c92a2e2efc11635dd4ad389cffc1575d437
Jens Christian Rasch <jcr@cloudrs.dk>
2018-02-22 12:32:21 +0100

    Added initial AI class

---
+++ app/src/main/java/org/example/AI/AIPlayer.java
@@ -0,0 +1,13 @@
+package org.example.AI;
+
+import org.example.enums.BoardStatus;
+
+/**
+ * Created by Jens Christian Rasch on 22-02-2018.
+ */
+
+public class AIPlayer {
+
+    private BoardStatus mySeed;
+    private BoardStatus opponentSeed;
+}
